224c7e21633329ac9a4d929ba5bcc65879abf4ce
lavanya <lavanyasliet@gmail.com>
2024-01-26 16:39:27 +0530

    first commit

---
+++ constant/baseurl.js
@@ -0,0 +1,3 @@
+const BASEURL = "https://bookstore-backend-g0u6.onrender.com";
+
+export default BASEURL;
---
+++ src/pages/DeleteBook.jsx
@@ -0,0 +1,57 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
+import BASEURL from "../../constant/baseurl";
+const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { enqueueSnackbar } = useSnackbar();
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`${BASEURL}/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar("Book Deleted Successfully", { variant: "success" });
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        // alert("An error happened, Please check console!");
+        enqueueSnackbar("Error", { variant: "error" });
+        console.log(error);
+      });
+  };
+  const handleNoDeleteBook = () => {
+
+    navigate("/");
+
+  };
+
+
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h2 className="text-2xl">Are you sure?</h2>
+        <div className="flex items-center gap-5">
+          <button className="p-10 bg-red-600" onClick={handleDeleteBook}>
+            Yes
+          </button>
+          <button className="p-10 bg-green-600" onClick={handleNoDeleteBook}>
+            No
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteBook;
